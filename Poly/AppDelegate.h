cf9c97e7023538c70f4738298bcc3274d75cc66f
Florian BUREL <florian.burel@mac.com>
2015-01-05 11:45:50 +0100

    Initial Commit

---
+++ Poly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Poly
+//
+//  Created by Florian BUREL on 05/01/2015.
+//  Copyright (c) 2015 Florian Burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
